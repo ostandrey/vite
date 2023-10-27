f14e08cecb2e0faff370eda91d930f2378b50723
ostandrey <ost.and.g@gmail.com>
2023-10-27 23:48:01 +0300

    import CSS modules

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 import './src/02';
 import collect from 'collect.js';
 import './src/assets/03.css';
-//server request, that return css file, contsin js code (generate automaticaly)
+//server request, that return css file, convert to js code (generate automaticaly)
 //read and transform css to js
+
+import styleA from './src/assets/04a.module.css';
+// import { heading as headingFontColor} from './src/assets/04a.module.css';
+import styleB from './src/assets/04b.module.css';
 
 console.log(collect([1, 3, 3, 7]).avg());
 
-console.log(styles); // string of export styles
+// console.log(styles); // string of export styles
 
 // const styleEl = document.createElement('style');
 // styleEl.innerText = styles;
 
 // document.querySelector('head').append(styleEl);
+
+console.log(styleA);
+console.log(styleB);
+
+document.querySelector('.heading').className = `${styleA.heading} ${styleB.heading}`;
