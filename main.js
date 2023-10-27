fb28d46467d249322842b05aef63dc9263339b04
ostandrey <ost.and.g@gmail.com>
2023-10-27 23:28:28 +0300

    import CSS files

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 import './src/02';
 import collect from 'collect.js';
+import './src/assets/03.css';
+//server request, that return css file, contsin js code (generate automaticaly)
+//read and transform css to js
 
 console.log(collect([1, 3, 3, 7]).avg());
+
+console.log(styles); // string of export styles
+
+// const styleEl = document.createElement('style');
+// styleEl.innerText = styles;
+
+// document.querySelector('head').append(styleEl);
